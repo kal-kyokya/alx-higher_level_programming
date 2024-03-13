e49ad613cf14b31fb7d83c25e031cda7acc4bf92
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-13 16:30:32 -0700

    Extract last digit of number, try 4.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if (number < 0):
-	remainder = (number % 10) * -1
+	remainder = ((number * -1) % 10) * -1
 else:
 	remainder = number % 10
-print("Last digit of {} is {}".format(number, remainder), end = ' ')
 if (remainder > 5):
-	print("and is greater than 5")
+	print(f"Last digit of {number} is {remainder} and is greater than 5")
 elif (remainder == 0):
-	print("and is 0")
+	print(f"Last digit of {number} is {remainder} and is 0")
 else:
-	print("and is less than 6 and not 0")
+	print(f"Last digit of {number} is {remainder} and is less than 6 and not 0")
