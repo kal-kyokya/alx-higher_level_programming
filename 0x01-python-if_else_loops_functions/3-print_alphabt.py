2f9543be019322cab748011f05fdf58627cfb9f8
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-13 17:40:13 -0700

    Print lowercases without e and q, try 1.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letter in (range(ord('a'), ord('z') + 1)):
-	if (letter == ord('e') or letter == ord('q')):
-		continue
-	print("{:c}".format(letter), end = '')
+    if (letter == ord('e') or letter == ord('q')):
+        continue
+    print("{:c}".format(letter), end = '')
