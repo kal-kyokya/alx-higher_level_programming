85ffc5a341a079fc4c8bef91470f618db9edfe4f
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-03-18 08:18:25 -0700

    Play fizzBuzz using python, try 2.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,9 @@
 def fizzbuzz():
     for num in range(1, 101):
         if (num % 3 == 0 and num % 5 == 0):
-            print("fizzBuzz", end=" ")
+            print("FizzBuzz", end=" ")
         elif (num % 3 == 0):
-            print("fizz", end=" ")
+            print("Fizz", end=" ")
         elif (num % 5 == 0):
             print("Buzz", end=" ")
         else:
