a5e7e397fd8d97908ebc779c9317933eeaa7d697
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-29 04:20:13 -0700

    Print last digit using modulo, try 3.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,6 @@ import random
 number = random.randint(-10000, 10000)
 if number < 0:
     last_d = (((number * -1) % 10) * -1)
-elif number == 0:
-    last_d = number
 else:
     last_d = number % 10
 print(f"Last digit of {number} is {last_d} and is ", end="")
@@ -13,4 +11,4 @@ if last_d > 5:
 elif last_d < 6 and last_d != 0:
     print("less than 6 and not 0")
 elif last_d == 0:
-    print("is 0")
+    print("0")
