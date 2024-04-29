6211a23e64e5ae6cd92c197bf6a6c254d6c24cd4
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-29 08:47:38 -0700

    Execute python's version of Fizzbuzz, try 1.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for count in range(1, 101):
         if count % 3 == 0 and count % 5 == 0:
-            print("Fizz Buzz", end=" ")
+            print("FizzBuzz", end=" ")
         elif count % 3 == 0:
             print("Fizz", end=" ")
         elif count % 5 == 0:
