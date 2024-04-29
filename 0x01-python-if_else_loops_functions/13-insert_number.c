b5d82324a8f5d5ff5d21d851df172ea3e7e329d2
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-29 11:38:00 -0700

    Add a node in a sorted Singly Linked List.

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_node - Insert a node accordingly in a sorted SLL.
+ * @head: Address of the head pointer.
+ * @number: Integer value of the node to be inserted.
+ *
+ * Return: A pointer to the inserted node.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node, *current;
+
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = number;
+	current = *head;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	while (current != NULL)
+	{
+		if (current->n > new_node->n && current == *head)
+		{
+			*head = new_node;
+			new_node->next = current;
+			break;
+		}
+		else if (current->n < new_node->n && new_node->n < current->next->n)
+		{
+			new_node->next = current->next;
+			current->next = new_node;
+			break;
+		}
+		current = current->next;
+	}
+
+	return (new_node);
+}
