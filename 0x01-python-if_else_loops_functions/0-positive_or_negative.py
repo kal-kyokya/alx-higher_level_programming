b581ba4d888e4b319d4fa4184ae17f223c55f323
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-29 03:49:07 -0700

    Write if...elif function, try 2.

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print(f"{number:i} is", end=" ")
+print(f"{number} is", end=" ")
 if number > 0:
     print("positive")
 elif number == 0:
