c2b1b9be61bcd4f3833de7dee7ccbe8919daaedb
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-30 16:02:13 -0700

    Print elements of a list, try 1.

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    '''
-    This function send elements of a list to stdout one by one, each on a new line
+    '''Prints list elements, each on a new line
 
-    my_list: The list of elements to be sent to stdout
+    Arg:
+        my_list: The list of elements to be sent to stdout
 
-    Return: Nothing.
+    Return:
+        Nothing.
     '''
     for num in my_list:
         print("{}".format(num))
-    
