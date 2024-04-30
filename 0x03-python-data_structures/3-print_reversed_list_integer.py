b95115a5d826dfea35237e8a55663ca12129b745
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-30 16:58:15 -0700

    Print list in reverse order, try 2.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,8 +8,5 @@ def print_reversed_list_integer(my_list=[]):
     Return:
         Nothing.
     '''
-    len = 0
-    for num in my_list:
-        len += 1
-    for count in range(len - 1, -1, -1):
+    for count in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[count]))
