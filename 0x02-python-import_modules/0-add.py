c097ffb78986885570813bd0a86d4a23ecd62580
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-30 06:02:32 -0700

    Import a function from the CWD without using __import__, try 2.

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add as sum
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, sum(a, b)))
+
+if __name__ == "__main__":
+    from add_0 import add as sum
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, sum(a, b)))
