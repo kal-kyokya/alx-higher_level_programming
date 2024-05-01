5624620ff70d401877142b1d825a01fa9b2afa88
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-04-30 17:40:41 -0700

    Use append list method.

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev_list = my_list[::-1]
-    for i in rev_list:
-        print("{:d}".format(i))
+    '''Prints a list in reverse order.
+
+    Arg:
+        my_list: List to be reversed and printed.
+
+    Return:
+        Nothing.
+    '''
+    for count in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[count]))
