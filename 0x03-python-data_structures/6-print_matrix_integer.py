a2cb1914b878ad74241e2ffd3236366cbaaabd7c
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-01 03:15:44 -0700

    Print a 2D list, try 1.

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -10,7 +10,7 @@ def print_matrix_integer(matrix=[[]]):
     '''
     for count1 in range(0, len(matrix)):
         for count2 in range(0, len(matrix[count1])):
-            print("{}".format(matrix[count1][count2]), end="")
+            print("{:d}".format(matrix[count1][count2]), end="")
             if count2 + 1 < len(matrix[count1]):
                 print(" ", end="")
         print()
