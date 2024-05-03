0023d2a998b07e5ca4f2e5779284f074a8d36a2b
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-03 05:36:02 -0700

    Multiply list integer values by input number x, try 3.

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    '''Multiplies all elements of a list by a specific input number.
-
-    Args:
-        my_list: List to be processed.
-        number: Integer by which list gets multiplied.
-
-    Return:
-        The new list version.
-    '''
     return list(map(lambda x: x * number, my_list))
