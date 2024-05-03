fcf10d8cf8e6805de6b9b155fdc6a0acf91636fe
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-03 05:34:21 -0700

    Multiply list integer values by input number x, try 2.

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -9,5 +9,4 @@ def multiply_list_map(my_list=[], number=0):
     Return:
         The new list version.
     '''
-#    return [x * number for x in my_list]
     return list(map(lambda x: x * number, my_list))
