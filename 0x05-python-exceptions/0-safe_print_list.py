2f26e774203e7b58155eea48109811dd269d5aed
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-08 07:46:44 -0700

    Deal with out of range print operations, try 5.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -13,7 +13,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[count]), end="")
         print()
         return count + 1
-    except:
+    except IndexError:
         count -= 1
         print()
         return count + 1
