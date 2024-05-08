4282943f399034765da7fc4a79988f41abd154ff
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-08 08:43:25 -0700

    Handle multiple exceptions, try 1.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -15,7 +15,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[x]), end="")
         except IndexError:
-            break;
+            break
         except ValueError:
             pass
         except Exception:
