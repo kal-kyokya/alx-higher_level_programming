2326e094644e6ab78760ede053e8c411f66de194
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-08 07:42:31 -0700

    Deal with out of range print operations, try 3.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,11 +6,11 @@ def safe_print_list(my_list=[], x=0):
         my_list: The list containing elements to be printed.
         x: The number of elements requested to be sent to stdout.
 
-    Return: The actual number of elements printed.
+    Return: The total number of elements printed.
     '''
     try:
         for count in range(0, x):
-            print(f"{my_list[count]}", end="")
+            print("{}".format(my_list[count]), end="")
         print()
         return count + 1
     except Exception:
