4bc907ce17de666910623e4c2cea6fc66b6792bd
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-09 06:27:04 -0700

    Task 102, try 2

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -17,7 +17,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             else:
                 result += (a ** b) / i
-        except:
+        except Exception:
             result = b + a
             break
 
