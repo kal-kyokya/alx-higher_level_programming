d00937b2b77853bde51e588159c7fa852731d92f
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-09 05:04:29 -0700

    Extract error message and print it to stderr, try 3.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -11,6 +11,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(int(value)))
         return (True)
-    except ValueError as err:
+    except Exception as err:
         print("Exception:", err)
         return (False)
