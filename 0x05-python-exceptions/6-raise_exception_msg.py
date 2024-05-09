10217974241e3c0cda7c2fe1ab1c635b671875ed
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-09 04:37:19 -0700

    Handle a raised exception with message.

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    '''Raises a name exception message with a specific message.
+
+    Arg:
+        The message to be sent to stdout.
+
+    Return:
+        Nothing.
+    '''
+    raise NameError(message)
