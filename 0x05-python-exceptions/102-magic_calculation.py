bc66e9d0f774825cca026be0c35ffa0fb46d32ad
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-09 06:24:42 -0700

    Task 102

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    '''Generated from its python bytecode.
+
+    Arg:
+        a: First argument value.
+        b: Second argument value.
+
+    Return:
+        The result of the defined operation.
+    '''
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except:
+            result = b + a
+            break
+
+    return (result)
