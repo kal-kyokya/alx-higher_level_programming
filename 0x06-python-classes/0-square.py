d737db126852702db157afdafc5329dad092729d
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-10 06:30:33 -0700

    Create an empty class, try 3.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 class Square():
-    """This class encapsulates all attributes
-    and methods needed for creation of a square.
-    """
+    """Facilitates creation of a square."""
     pass
