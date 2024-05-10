ba9090bcebfea03c471a667d8ff5d383567a77a3
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-10 15:42:15 -0700

    Task 103, try 3.

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,54 +1,22 @@
 #!/usr/bin/python3
-
-"""Module Docstring.
-This module demostrates how to create a class named Square
-that defines a square.
-
-"""
+""" Docstring """
+from math import pi
 
 
-class Square:
-    """Defines a square
-    Args:
-        No parameters
-    Attributes:
-        No attributes
+class MagicClass:
+    """setting up magic"""
 
-    """
-    def __init__(self, size=0):
-        """Initializes a new square
-        Args:
-            size (int): size of the square"""
-        self.__size = size
-        if not type(self.__size) is int:
-            raise TypeError("size must be an integer")
-        elif self.__size < 0:
-            raise ValueError("size must be >= 0")
-
-    @property
-    def size(self):
-        """ getter function to set the size
-            Returns:
-                size
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """ setter function to assign value to size
-            Args:
-                value (int): value to be assigned to size
-        """
-        if not type(value) is int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+    def __init__(self, radius=0):
+        """ Another docstring """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
 
     def area(self):
-        """Calculates the current square area
-        Args:
-        Returns:
-        Examples:
-        """
-        return self.__size ** 2
+        """with the docstring"""
+        return self.__radius ** 2 * pi
+
+    def circumference(self):
+        """Docstring"""
+        return 2 * pi * self.__radius
