52fe1e69a1d54e36bf8b4eddf42c75695d47d541
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-13 06:15:15 -0700

    Print file's content, try 4.

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,33 +1,30 @@
 #!/usr/bin/python3
-"""
 
-prints first name and last name
-
-"""
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """ no modules
+    """Print text with two new lines after each '.', '?', and ':'.
 
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
     """
-
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    line = ""
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    for char in text:
-        line += char
-
-        if char in ".?:":
-            print(line.strip())
-            print()
-            line = ""
-
-    if line.strip():
-        print(line.strip())
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
