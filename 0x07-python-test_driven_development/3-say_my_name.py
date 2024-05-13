0bdf7aa63627664b738e3a8e74f758f43284d6f4
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-13 05:10:13 -0700

    Create a name printing function and its test file, try 2.

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -28,13 +28,13 @@ def say_my_name(first_name, last_name=""):
     if ((len(first_name) == 0 or
             all(char == ' ' for char in first_name)) and
             len(last_name) != 0):
-        print(f"{last_name}")
+        print(f"My name is {last_name}")
     elif ((len(last_name) == 0 or
             all(char == ' ' for char in last_name)) and
             len(first_name) != 0):
-        print(f"{first_name}")
+        print(f"My name is {first_name}")
     else:
-        print(f"{first_name} {last_name}")
+        print(f"My name is {first_name} {last_name}")
 
 
 if __name__ == ("__main__"):
