e0fdac95df12784e16015d77688b678bd04b584b
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-15 04:51:41 -0700

    Create an empty rectangle class, try 1.

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-'0-rectangle' is Rectangle creations module. 
+'0-rectangle' is a Rectangle creation module. 
 """
 
 
 class Rectangle:
+    """
+    This is the blueprint for all its instances of the 'Rectangle'.
+    """
     pass
 
