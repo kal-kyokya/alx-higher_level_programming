2d31d7f8540a37bafdb2005ca98e86f39aa914ff
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-17 06:01:00 -0700

    Cleaning up Alx Higher level programming repository.

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-'101-locked_class' is class creation module
+'101-locked_class' is class creation module.
 """
 
 
 class LockedClass:
-    """Creates a class restricting dynamic initializations
-    of new attributes unless named 'first_name'.
-    """
+    """Class that restricts dynamic creation of unspecified attributes."""
     __slots__ = ['first_name']
 
     def __init__(self):
--- 0x09-python-everything_is_object/101-main.py
+++ 0x09-python-everything_is_object/101-main.py
@@ -6,8 +6,21 @@ lc = LockedClass()
 print("\n", LockedClass.__dict__)
 
 lc.first_name = "John"
+print("\n", lc.first_name)
+print("\n", LockedClass.__dict__)
+
 try:
     lc.last_name = "Snow"
 except Exception as e:
     print()
     print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    del lc.first_name
+except Exception as err:
+    print(err)
+
+try:
+    print(lc.first_name)
+except Exception as err:
+    print(err)
