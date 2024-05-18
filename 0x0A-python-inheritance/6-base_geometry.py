c1155e7216366d4d325992a7b4933b49bc969f32
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-18 16:42:29 -0700

    Create a public instance method.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+'6-base_geometry' is a class creation module.
+"""
+
+
+class BaseGeometry:
+    """Empty class"""
+    def __init__(self):
+        """Constructs and Initializes any class' instance."""
+        pass
+
+    def area(self):
+        """Raises an Exception with a message."""
+        raise Exception("area() is not implemented")
