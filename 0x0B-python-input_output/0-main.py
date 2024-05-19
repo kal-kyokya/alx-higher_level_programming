f396f2c53eecde3d10802a27846ff3e47cc77d74
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-19 11:29:22 -0700

    Read from a file and print content.

---
+++ 0x0B-python-input_output/0-main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+read_file = __import__('0-read_file').read_file
+
+print(";-) " * 25, "\n")
+
+read_file("file0.txt")
+
+print(";-) " * 25, "\n")
+
+read_file("0-read_file.py")
+
+print(";-) " * 25, "\n")
+
+try:
+    read_file()
+except Exception as err:
+    print(err)
+
+print(";-) " * 25, "\n")
+
+try:
+    read_file("")
+except Exception as err:
+    print(err)
