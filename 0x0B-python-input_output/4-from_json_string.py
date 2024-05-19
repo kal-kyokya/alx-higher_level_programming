0f915f0e46ae39a5b7f6c082829fa57611a9e6f5
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-19 13:00:31 -0700

    Decode a JSON representation into a python object.

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+'4-from_json_string' is a python object Decoding.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Converts a JSON representation into an equivalent python object.
+
+    Arg:
+        my_str: JSON representation to be converted.
+
+    Return:
+        The Python object.
+    """
+    with open("JavaSON.json", "r") as json_file:
+        json_file = json.loads(my_str)
+    return(json_file)
