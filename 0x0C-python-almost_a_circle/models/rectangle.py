4c4f6b9b666022f5a9d8d0f21282287902edb48f
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-21 12:18:17 -0700

    Create a child class to base named rectangle, try 2.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -27,3 +27,55 @@ class Rectangle(Base):
         self.__height = input_h
         self.__x = input_x
         self.__y = input_y
+
+    @property
+    def width(self):
+        """Getter for the attribute 'width'.
+
+        The setter follows right after.
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, input_value):
+        self.__width = input_value
+        return (self.__width)
+
+    @property
+    def height(self):
+        """Getter for the attribute 'height'.
+
+        The setter follows right after.
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, input_value):
+        self.__height = input_value
+        return (self.__height)
+
+    @property
+    def x(self):
+        """Getter for the attribute 'x'.
+
+        The setter follows right after.
+        """
+        return (self.__x)
+
+    @x.setter
+    def x(self, input_value):
+        self.__x = input_value
+        return (self.__x)
+
+    @property
+    def y(self):
+        """Getter for the attribute 'y'.
+
+        The setter follows right after.
+        """
+        return (self.__y)
+
+    @y.setter
+    def y(self, input_value):
+        self.__y = input_value
+        return (self.__y)
