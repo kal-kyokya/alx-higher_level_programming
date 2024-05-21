32b5dd6a6392125c0ac1b6c76615fa9f807c9753
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-05-21 13:15:07 -0700

    Add functionalities to exisiting method.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -118,7 +118,11 @@ class Rectangle(Base):
 
     def display(self):
         """Represents the rectangle using the character '#'."""
+        for y in range(self.y):
+            print()
         for h in range(self.height):
+            for x in range(self.x):
+                print(end=" ")
             for w in range(self.width):
                 print("#", end="")
             print()
