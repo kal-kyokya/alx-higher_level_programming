a1f25fd24dd275d918ed3510ebc6bfbdcb12a331
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-14 14:01:32 -0700

    Create table and its rows.

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- Script creating table and records
+
+-- Command creating the table
+CREATE TABLE IF NOT EXISTS second_table (
+       id INT,
+       name VARCHAR(256),
+       score INT
+);
+
+-- Command creating/Inserting records for 'second_table'
+INSERT INTO second_table
+(id, name, score)
+VALUES (1, 'John', 10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
