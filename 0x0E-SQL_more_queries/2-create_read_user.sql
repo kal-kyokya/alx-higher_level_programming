51bf01c2be38521a84e52ae689b3535608f51b21
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-16 08:22:06 -0700

    Create a read only user, try 1.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- Script creating a Database and a User with 'SELECT' granted.
 
 -- Command for Database creation
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
 -- Command creating the User
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
