168e010877bcecb10fbd23a1903b4e2d9d63f6c3
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-16 08:08:56 -0700

    Create a mysql user with all privileges.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,3 +2,6 @@
 
 -- Command for user creation
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Command granting all privileges to create user
+GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost';
