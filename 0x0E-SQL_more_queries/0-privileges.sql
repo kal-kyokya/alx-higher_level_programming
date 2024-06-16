65535a36b9e1e3cb85ce44e897a663b0d685a5a4
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-16 08:01:03 -0700

    Print user privileges, try 2.

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,10 +1,8 @@
 -- Script displaying all granted privileges to user John Doe.
 
 -- Command doing the display
-SELECT GRANT PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost'
+SHOW GRANTS
+FOR 'user_0d_1'@'localhost'
 AND
-GRANT PRIVILEGES
-ON *.*
-TO 'user_0d_2'@'localhost';
+GRANTS
+FOR 'user_0d_2'@'localhost';
