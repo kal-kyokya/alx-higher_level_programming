db857e8bff964434fe8f2cee10daadc68e77c212
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-17 04:26:46 -0700

    Create database and table with constraints, try 2.

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,7 +6,7 @@ IF NOT EXISTS `hbtn_0d_usa`;
 
 -- Command creating the table
 CREATE TABLE
-IF NOT EXISTS `hbtn_0d_usa.states` (
+IF NOT EXISTS `hbtn_0d_usa`.`states` (
    id INT AUTO_INCREMENT NOT NULL,
    name VARCHAR(256) NOT NULL,
    PRIMARY KEY (id)
