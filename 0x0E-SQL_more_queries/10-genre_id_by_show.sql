b8b186480236b092460f8a1ffbbe255a78674d8c
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-17 05:48:58 -0700

    Left Join tables experiment, try 1.

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -6,4 +6,4 @@ FROM `tv_shows`
 LEFT JOIN `tv_show_genres`
 ON `tv_shows`.`id`, `tv_show_genres`.`show_id`
 WHERE `tv_show_genres`.`genre_id` NOT NULL
-ORDER BY `tv_shows`.`title` AND `tv_show_genres`.`genre_id`;
+ORDER BY `tv_shows`.`title`, `tv_show_genres`.`genre_id`;
