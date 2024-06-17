8ba6c8c0e8b479fd72fdc8e9c8023fa549e77cf9
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-17 05:26:43 -0700

    Inner Join 2 tables.

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,8 @@
+-- Script listing rows of a field
+
+-- Command doing the listing
+SELECT `cities`.`id`, `cities`.`name`, `states`.`name`
+FROM `cities`
+INNER JOIN `states`
+ON `states`.`id` = `cities`.`state_id`
+ORDER BY `cities`.`id`;
