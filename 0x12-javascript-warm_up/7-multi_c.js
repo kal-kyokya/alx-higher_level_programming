6b9abbe4a99c812340e4b1690a9070e1b6d5486c
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-18 16:23:09 -0700

    Combine conditional statement and loop, try 1.

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,7 @@ if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   let count = 0;
-  while (count < parseInt(process.argv[2]))
-  {
+  while (count < parseInt(process.argv[2])) {
     console.log('C is fun');
     count++;
   }
