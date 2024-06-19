4a48b442e33b9c10498bffc4a7c80ec1513eb08d
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-19 16:20:54 -0700

    Print class instance using Capital x: 'X'.

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let count = 0;
+    while (count < this.height) {
+      console.log('X'.repeat(this.width));
+      count++;
+    }
+  }
+};
