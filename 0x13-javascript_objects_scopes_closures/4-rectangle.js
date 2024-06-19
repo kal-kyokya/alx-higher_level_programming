8a58e30e3713b7db6a117575f0b0e576e9fdff68
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-19 16:40:01 -0700

    Implement more instance methods.

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectngle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let count = 0;
+    while (count < this.height) {
+      console.log('X'.repeat(this.width));
+      count++;
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
