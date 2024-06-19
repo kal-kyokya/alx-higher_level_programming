f3b19793c70167c2285eeeac5d30efad243665e9
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-19 05:24:03 -0700

    Task 5 retry.

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-if (isNaN(Number(process.argv[2]))) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ', Number(process.argv[2]));
-}
+const { argv } = require('process');
+const converted = Number(argv[2]);
+if (isNaN(converted)) { console.log('Not a number'); } else { console.log(`My number: ${converted}`); }
