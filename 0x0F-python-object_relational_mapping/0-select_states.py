d8c7526320857ee17a18b968bc23638ca640e065
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-25 05:11:32 -0700

    Query table using 'MySQLdb'.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+'0-select_states' the module MySQLdb to query all states found in our Database
+
+Usage:
+./0-select_states <mysql username> <mysql password> <database name>
+"""
+import MySQLdb
+import sys
+
+
+if "__name__" == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states"
+    cursor.execute(query)
---
+++ 0x0F-python-object_relational_mapping/0-select_states.sql
@@ -0,0 +1,23 @@
+-- Create a Database, sets it as 'in use', Add a table to it with associated values
+
+-- Database creation
+CREATE DATABASE
+IF NOT EXISTS hbtn_0e_0_usa;
+
+-- Setting created Database 'in use'
+USE hbtn_0e_0_usa;
+
+-- Table creation
+CREATE TABLE
+IF NOT EXISTS states (
+   id INT     NOT NULL AUTO_INCREMENT,
+   name VARCHAR(256)   NOT NULL,
+   PRIMARY KEY (id)
+);
+
+-- Rows/Record addition to Table
+INSERT INTO states (name)
+VALUES ("Congo"), ("Kenya"), ("Rwanda"),
+       ("Burundi"), ("Uganda"), ("Tanzania"),
+       ("Nigeria"), ("Zambia"), ("Zimbabwe"),
+       ("Tchad"), ("Somaliland"), ("Ethiopia");
