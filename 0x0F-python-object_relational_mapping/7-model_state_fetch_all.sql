428ff46708c779527a75e104c082a4dd0abe4d4d
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-28 13:02:09 -0700

    Select table records using SQLAlchemy.

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -0,0 +1,8 @@
+-- Insert states inside the 'states Table'
+
+INSERT INTO
+states (name)
+VALUE ("Congo"), ("Rwanda"), ("Burundi"),
+      ("Uganda"), ("Tanzania"), ("Kenya"),
+      ("Zambia"), ("Ethiopia"), ("Somalia"),
+      ("Zimbabwe"), ("Tchad"), ("Somaliland");
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -20,6 +20,5 @@ class State(Base):
 
     __tablename__ = "states"
 
-    id = Column(Integer, nullable=False,
-                unique=True, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
