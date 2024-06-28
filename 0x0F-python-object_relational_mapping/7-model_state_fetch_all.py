c4a0c1087495bd9d39d8cbda6cc1271b5d4f3b66
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-28 13:20:36 -0700

    Select table records using SQLAlchemy, try 2: Remove 'imported Base'.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,7 +3,7 @@
 '7-model_state_fetch_all' Retrieves all
 states stored inside the 'State Table'
 """
-from model_state import Base, State
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import sys
@@ -14,8 +14,6 @@ if __name__ == "__main__":
         "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
