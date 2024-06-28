87064ba5383dcf983f705aead564d0a050e59a2c
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-28 13:10:32 -0700

    Select table records using SQLAlchemy, try 1: add 'if __name__' clause.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,16 +9,17 @@ from sqlalchemy import create_engine
 import sys
 
 
-engine = create_engine(
-    "mysql+mysqldb://{}:{}@localhost/{}".format(
-        sys.argv[1], sys.argv[2], sys.argv[3]),
-    pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
 
-Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-states = session.query(State).all()
-for state in states:
-    print(state.id, ": ", state.name)
+    states = session.query(State).all()
+    for state in states:
+        print(state.id, ": ", state.name)
