60bbf4c47f43fc2263ea1b6b99e0776a92b016bc
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-28 13:37:06 -0700

    Select table records using SQLAlchemy, try 3: use 'query().order_by()'.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -13,11 +13,12 @@ if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(
             sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
+        pool_pre_ping=True
+    )
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
     states = session.query(State).order_by(State.id)
     for state in states:
-        print(state.id, ": ", state.name)
+        print("{}: {}".format(state.id, state.name))
