b0d051960b0e00bdc3af2602c08d13796f3e6998
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-06-28 13:53:53 -0700

    Use 'query().first' to return table record.

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+'8-model_state_fetch_first' Retrieves a table element
+"""
+import sys
+from model_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
+
+    state = session.query(State).first()
+    print(f"{state.id}: {state.name}")
