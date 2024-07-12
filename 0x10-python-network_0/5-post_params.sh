17a4e77f8d15127c7954380faa14d500e2e140cb
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-12 14:33:54 -0700

    Display the status code of a response using 'curl -w'.

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a POST request with additional query parameters
-curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=demtest@gmail.com&subject=Dem will always be here for DAMSKAL" "$1"
