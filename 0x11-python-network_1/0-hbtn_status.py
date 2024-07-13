239624a104a614fae7e9589f7926005542b50845
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-13 12:31:22 -0700

    Use the 'urllib' package to fetch a URL 1.2.0: The Pato Way.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-# Fetch a url using the 'urllib' package
-import urllib.request as req
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
+import urllib.request
 
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    data = response.read()
 
-if __name__ == "__main__":
-    url = "http://kalkyokya.tech/"
-    with req.urlopen(url) as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(content.decode(encoding="utf-8")))
+    print("Body response:")
+    print("\t- type:", type(data))
+    print("\t- content:", data)
+    print("\t- utf8 content:", data.decode('utf-8'))
