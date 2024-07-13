36f65aedcc1a5cc4354e4b258a74526b69e465ac
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-13 12:22:09 -0700

    Use the 'urllib' package to fetch a URL.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,11 @@
-#!/usr/bin//python3
-"""
-'0-hbtn_status_py' fetches a url using the 'urllib' package
-"""
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
 from urllib import request
 
-
 if __name__ == "__main__":
-    url = "http://kalkyokya.tech/"
-    with request.urlopen(url) as response:
-        content = response.read()
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
+        print("\t- type: {}".format(type(response)))
         print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(content.decode(encoding="utf-8")))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
