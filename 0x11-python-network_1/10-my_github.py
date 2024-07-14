d2460bfe9dbe0c64d9256df2208e07c273083324
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-14 05:48:32 -0700

    Utilize Basic Authentication to retrieve Github user's ID.

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
-#
+#!/usr/bin/python3
+"""
+'10-my_github' Uses 'Basic Authentication' to access a GitHub account
+through the 'requests' module's class 'HTTPBasicAuth'.
+"""
+from requests.auth import HTTPBasicAuth as HBA
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    rest_api = "https://api.github.com/user"
+
+    response = requests.get(rest_api, auth=HBA(username, password))
+    content = response.json()
+    print(content.get('id'))
