eccf0c5881f717ce17a6a31146dd76709416f250
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-14 06:17:46 -0700

    Handle deeper level GitHub api url and extract commits from a repo.

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+'100-github_commits' Returns the 10 most recent GitHub repo commits.
+"""
+from requests.auth import HTTPBasicAuth as HBA
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[1]
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits?per_page=10"
+    rest_api = url
+
+    response = requests.get(rest_api)
+    json_content = response.json()
+
+    for commit in json_content:
+        sha = commit['sha']
+        author = commit['commit']['author']['name']
+        print("{}: {}".format(sha, author))
