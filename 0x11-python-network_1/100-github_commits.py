1f184dc430c91717d2a809552bc507886fa35a0f
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-14 06:27:46 -0700

    Handle deeper level GitHub rest api to extract commits from repo.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,14 +9,17 @@ import sys
 
 if __name__ == "__main__":
     repo = sys.argv[1]
-    owner = sys.argv[1]
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits?per_page=10"
-    rest_api = url
+    owner = sys.argv[2]
+    rest_api = "https://api.github.com/repos/{}/{}/commits".format(
+        owner, repo)
 
     response = requests.get(rest_api)
-    json_content = response.json()
+    commits = response.json()
 
-    for commit in json_content:
-        sha = commit.get('sha')
-        author = commit.get('commit').get('author').get('name')
-        print("{}: {}".format(sha, author))
+    try:
+        for i in range(10):
+            sha = commits[i].get('sha')
+            author = commits[i].get('commit').get('author').get('name')
+            print("{}: {}".format(sha, author))
+    except IndexError:
+        pass
