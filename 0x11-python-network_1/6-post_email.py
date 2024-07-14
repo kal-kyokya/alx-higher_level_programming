9ec6fb410092a167aee5a20ec66e73f105067673
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-07-13 17:04:29 -0700

    Send a 'POST' request using the 'requests' package in Python.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+'6-post_email' Reads a URL and an email from the cmd line and
+POST them using the 'requests' package.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    query_str = {'email': email}
+    response = requests.post(url, data=query_str)
+    content = response.text
+    print("Your email is:", content)
