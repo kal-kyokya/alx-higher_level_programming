a1ce75fd395223836d18905117478ba277209aa0
kal-kyokya <kalkyokya4@gmail.com>
2024-08-04 04:04:04 -0700

    Use 'process.argv' and 'fs' module to read a cmd line argument.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const fileSystem = require('fs');
 
 fileSystem.readFile(process.argv[2], 'UTF-8', (err, fileContent) => {
-    if (err) {
-	console.log('Error occured during reading of file');
-	process.exit(1);
-    }
-    console.log(fileContent);
+  if (err) {
+    console.log('Error occured during reading of file');
+    process.exit(1);
+  }
+  console.log(fileContent);
 });
