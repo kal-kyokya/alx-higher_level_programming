f2e76b5f1b0efa69300d7d189d5e83a4e29ba389
kal-kyokya <kalkyokya4@gmail.com>
2024-08-04 05:52:32 -0700

    Request JSON resource and parse it using 'JSON.parse'.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,12 +4,12 @@ const request = require('request');
 
 const episodeID = process.argv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/';
-resource = url + episodeID;
+const resource = url + episodeID;
 request(resource, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     process.exit(1);
   }
-  parsedObj = JSON.parse(body);
+  const parsedObj = JSON.parse(body);
   console.log(parsedObj.title);
 });
