67c113a479724c1737d03b41e12ecd1d43b5ebdf
kal-kyokya <kalkyokya4@gmail.com>
2024-08-04 05:49:07 -0700

    Request JSON resource and parse it using 'JSON.parse'.

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Prints title of a Star Wars movies based on the episode number
+const request = require('request');
+
+const episodeID = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+resource = url + episodeID;
+request(resource, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+    parsedObj = JSON.parse(body);
+    console.log(parsedObj.title);
+});
